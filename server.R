3a4dd03fa888ce8cff46a02f7c2c50bdffe2ed5c
a-thind <26285885+a-thind@users.noreply.github.com>
2022-07-31 01:09:13 +0000

    Allow IBD region filtering and gene name and rs links

---
+++ server.R
@@ -0,0 +1,101 @@
+library(jsonlite)
+# https://www.ncbi.nlm.nih.gov/gene?term=(human%5BOrganism%5D)%20AND%20BRCA1%5BGene%20Name%5D
+# Define server logic
+server <- function(input, output, session) {
+  options(shiny.maxRequestSize=1000*1024^2)
+
+  shinyDirChoose(input, "sh_outdir", roots=c(wd='.'))
+
+  # start pipeline
+  svvcf <- reactive({
+    req(input$vcf)
+    ext=tools::file_ext(input$vcf$name)
+    switch(
+      vcf.gz=input$vcf$datapath,
+      vcf=input$vcf$datapath,
+      validate("Input file is not a VCF file.")
+    )
+  })
+
+  # Short Variants tab
+  #-----------------------
+  # read short variants csv
+  col_types <- list(CHROM='f', ID='c', REF='c', ALT='c', FILTER='f', ALLELE='f',
+                    CONSEQUENCE='c', IMPACT='f', SYMBOL='c', GENE='c',
+                    FEATURE_TYPE='f', FEATURE='c', BIOTYPE='f', EXON='c',
+                    INTRON='c', POS='d', QUAL='d', AC='d', AF='d', AN='d',
+                    DP='d', FS='d', MQ='d', MQRANKSUM='d', QD='d',
+                    READPOSRANKSUM='d', SQR='d', CDNA_POSITION='d',
+                    CDS_POSITION='d', PROTEIN_POSITION='d', DB='f',
+                    END='d', DISTANCE='d', FLAGS='f', MAX_AF_POPS='d',
+                    SOMATIC='d', MOTIF_NAME='c', MOTIF_POS='d', HIGH_INF_POS='d',
+                    MOTIF_SCORE_CHANGE='d', TRANSCRIPTION_FACTORS='c',
+                    CADD_PHRED='d', HGVSC='c', HGVSP='c', PUBMED='c',
+                    POLYPHEN_CALL='f',SIFT_CALL='f', CADD_RAW='d', SOR='d',
+                    CODONS='c', STRAND='f', SIFT_SCORE='d', POLYPHEN_SCORE='d',
+                    MAX_AF='d', EXISTING_VARIATION='c', HGNC_ID='c',
+                    CLIN_SIG='f', SYMBOL_SOURCE='f', PHENO='f', AMINO_ACIDS='c',
+                    NEAREST='c', HGVS_OFFSET='d'
+                    )
+
+  short_data <- reactive({
+    req(input$short_tsv)
+    ext=tools::file_ext(input$short_tsv$name)
+    switch(ext,
+           tsv=vroom::vroom(input$short_tsv$datapath, delim="\t",
+                            col_names=TRUE),
+           txt=vroom::vroom(input$short_tsv$datapath, delim="\t", 
+                            col_names=TRUE),
+           validate("Invalid file: Please upload a tsv/text file")
+    )
+    
+  })
+
+  ibd_data <- reactive({
+    req(input$ibd_seg)
+    ext=tools::file_ext(input$ibd_seg$name)
+    switch(ext,
+           seg=input$ibd_seg$datapath,
+           validate("Invalid file: Please upload an IBIS IBD segment file (.seg)")
+    )
+  })
+
+
+  # render ideogram
+  output$ideogram_plot <- renderIdeogram({
+    ideogram({ibd_data()})
+  })
+  
+  # reactive expression for ibd region filtering
+    ibd_filter <- reactive({
+        req(input$chosenRegion)
+        short_data() %>% 
+          filter(CHROM==paste0('chr', input$chosenRegion$chr) &
+                   (POS >= input$chosenRegion$start) &
+                    (POS <= input$chosenRegion$stop))
+    })
+
+  output$chosenRegion <- renderText({
+    req(input$chosenRegion)
+    paste(input$chosenRegion$start, input$chosenRegion$stop, input$chosenRegion$chr)
+    #typeof(input$chosenRegion$start)
+    #class(input$chosenRegion$start)
+  })
+  
+  # render short variants table
+  output$short_tab <- renderDT({ DT::datatable(ibd_filter() %>%
+                       # create link for gene symbols to NCBI gene db
+                           mutate(
+                             SYMBOL=ifelse(!is.na(SYMBOL), paste0('<a href="https://www.ncbi.nlm.nih.gov/gene?term=(human[Organism]) AND ', SYMBOL, '[Gene Name]">', SYMBOL,'</a>'), SYMBOL)) %>%
+                        mutate(
+                          RS=ifelse(!is.na(RS), paste0('<a href="https://www.ncbi.nlm.nih.gov/snp/?term=', RS, '">', RS, '</a>'), RS)
+                        ) %>%
+                         select(ID, RS, HGVSC, HGVSP,
+                              SYMBOL, CONSEQUENCE, MAX_AF, IMPACT, 
+                              CADD_PHRED, POLYPHEN_CALL, 
+                              SIFT_CALL, CLNSIG), escape=FALSE, 
+                      rownames=FALSE)
+  })
+  
+}
+
---
+++ ui.R
@@ -0,0 +1,112 @@
+# Shiny App
+#
+
+library(shiny)
+library(shinydashboard)
+library(shinyFiles)
+library(biomaRt)
+library(ideogram)
+library(DT)
+
+# csqs <- c("missense", "frame shift", "stop gain", "stop loss")
+# vep_impact <- c("high", "moderate", "low")
+
+# make_link <- function(gene_id) {
+#   sprintf(
+#     "<a href=https://www.ensembl.org/Homo_sapiens/Gene/Summary?db=core;g=%s>%s</a>",
+#     gene_id, gene_id)
+# }
+
+# source, output folder and config
+#
+
+# Define UI for application
+# sidebar
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Start Pipeline", tabName = "pipeline"),
+    menuItem("Short Variants", tabName="short_vars")
+  )
+)
+# dashboard body
+body <- dashboardBody(
+  tabItems(
+    # Starting pipeline
+    #----------------------
+    tabItem(
+      tabName="pipeline",
+      fluidRow(
+        box(title="Short Variants",
+            status="primary",
+            column(8,
+              fileInput("sh_vcf", "Upload input short variants VCF file (compressed)",
+                             accept=c("vcf.gz"), width="75%"),
+              shinyDirButton("sh_outdir", "Select output folder",
+                                    title="Output folder", multiple=F),
+              textInput("email", "Enter an email address:", width="75%"),
+              actionButton("sh_start", "Start")
+              )
+            ),
+
+            box(title="Advanced Configuration", collapsible = TRUE,
+                collapsed = TRUE,
+                status = "warning")
+
+      ),
+      fluidRow(
+        box(title="Structural Variants",
+            status = "primary",
+            fileInput("sv_vcf", "Upload input structural variants VCF file (compressed).",
+                      accept=c("vcf.gz"), width="50%")
+        )
+      )
+
+    ),
+    # Short variants tab
+    #-----------------------
+    tabItem(
+      tabName="short_vars",
+      fluidRow(
+        box(
+          title = "IBD regions Ideogram",
+          status="primary",
+          width = 8,
+          height="600px",
+          box(
+            width = 12,
+            height="535px",
+            ideogramOutput("ideogram_plot")
+          )
+
+        ),
+        box(
+          title="Files",
+          width=4,
+          fileInput("short_tsv", "Upload pipeline output file (.tsv/.txt)",
+                    accept=c(".tsv", ".txt")),
+          fileInput("ibd_seg", "Upload IBIS IBD segment file (.seg)",
+                    accept=c(".seg")),
+          status = "primary",
+          verbatimTextOutput("chosenRegion")
+        )
+      ),
+      fluidRow(
+        box(
+          width=12,
+          height="100%",
+          actionButton("reset_short_tab", "Reset"),
+          downloadButton("Download"),
+          DTOutput("short_tab"),
+          status = "primary"
+        )
+      )
+    )
+  )
+)
+
+# define user interface
+ui <- dashboardPage(
+  dashboardHeader(title="IBD Variants"),
+  sidebar,
+  body
+)
